b47cf548d06bd8c46db8656fe2b9d239cd504ad7
Samuel Oyedeji <32018370+sammyteeoye@users.noreply.github.com>
2021-10-16 20:35:25 +0100

    personal portfolio project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#FF2323',
+          200: '#FF2323',
+        }
+      },
+      fontFamily:{
+        body:['Nunito']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
